3059b9c1c66b73ee7138d32e4d547862c6036895
dtbead <113726875+dtbead@users.noreply.github.com>
2025-06-04 01:27:35 -0500

    [file_service] added NewFile and DeleteFile

--- internal/service/file/file.go
+++ internal/service/file/file.go
@@ -17,10 +17,10 @@ func NewService(FileRepo storage.FileRepository) *FileService {
 }
 
 func (f FileService) NewFile(ctx context.Context, file io.Reader, extension string) (file_id entities.FileID, err error) {
-	panic("unimplemented")
+	return f.FileRepo.NewFile(ctx, file, extension)
 }
 func (f FileService) DeleteFile(ctx context.Context, file_id entities.FileID) (err error) {
-	panic("unimplemented")
+	return f.FileRepo.DeleteFile(ctx, file_id)
 }
 
 func (f FileService) NewTempFile(ctx context.Context) (file io.ReadWriteCloser, err error) {
