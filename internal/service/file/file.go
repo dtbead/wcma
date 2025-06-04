8b79f4452da1d998e684a7eb41e8e38eea1b9557
dtbead <113726875+dtbead@users.noreply.github.com>
2025-06-04 01:26:20 -0500

    [file_service] fixed not being able to call interface methods

--- internal/service/file/file.go
+++ internal/service/file/file.go
@@ -9,11 +9,11 @@ import (
 )
 
 type FileService struct {
-	FileRepo *storage.FileRepository
+	FileRepo storage.FileRepository
 }
 
 func NewService(FileRepo storage.FileRepository) *FileService {
-	return &FileService{FileRepo: &FileRepo}
+	return &FileService{FileRepo: FileRepo}
 }
 
 func (f FileService) NewFile(ctx context.Context, file io.Reader, extension string) (file_id entities.FileID, err error) {
@@ -24,7 +24,7 @@ func (f FileService) DeleteFile(ctx context.Context, file_id entities.FileID) (e
 }
 
 func (f FileService) NewTempFile(ctx context.Context) (file io.ReadWriteCloser, err error) {
-	return f.NewTempFile(ctx)
+	return f.FileRepo.NewTempFile(ctx)
 }
 
 func (f FileService) GetHash(ctx context.Context, file_id entities.FileID) (err error, hashes entities.Hashes) {
